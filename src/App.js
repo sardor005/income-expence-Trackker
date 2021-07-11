c571da5d3949b96c7a1b1470b206d7c3302178ac
Sardor <sardorbey2697@gmail.com>
2021-07-11 10:39:46 +0500

    Speechly AI voice trackker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Grid } from '@material-ui/core'
+import Details from './components/Details/Details'
+import useStyles from './styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+  const classes = useStyles()
+    return (
+      <div>
+        <Grid
+          classNmae={classes.grid}
+          container
+          spacing={0}
+          alignItems="center"
+          justify="center"
+          style={{ height: "100vh" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Grid item xs={12} sm={4}>
+            <Details title='Income'/>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            Main
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Details title='Expense' />
+          </Grid>
+        </Grid>
+      </div>
+    );
 }
 
-export default App;
+export default App
